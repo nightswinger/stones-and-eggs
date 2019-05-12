b206d6a5598ee711c6c3d3b66322c60a46b8f6d8
nightswinger <stardustkids83@gmail.com>
2019-05-12 12:54:33 +0900

    change field name array to v

--- intarray/int_array.go
+++ intarray/int_array.go
@@ -5,34 +5,38 @@ import (
 )
 
 type IntArray struct {
-	array []int
+	v []int
 }
 
 func New(i []int) *IntArray {
-	a := IntArray{array: i}
+	a := IntArray{v: i}
 	return &a
 }
 
 func (a *IntArray) Get(index int) int {
-	return a.array[index]
+	return a.v[index]
 }
 
 func (a *IntArray) Set(index, value int) {
-	a.array[index] = value
+	a.v[index] = value
 }
 
 func (a *IntArray) Push(i int) {
-	a.array = append(a.array, i)
+	a.v = append(a.v, i)
 }
 
 func (a *IntArray) Min() int {
-	return Min(a.array)
+	return Min(a.v)
 }
 
 func (a *IntArray) Max() int {
-	return Max(a.array)
+	return Max(a.v)
+}
+
+func (a *IntArray) Val() []int {
+	return a.v
 }
 
 func (a *IntArray) String() string {
-	return fmt.Sprintf("%v", a.array)
+	return fmt.Sprintf("%v", a.v)
 }
